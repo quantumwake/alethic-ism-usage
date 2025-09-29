e8f7dc5632ee6baa8acc9fec14311c5e4a5b2de0
Kasra Rasaee <krasaee@gmail.com>
2025-09-29 12:58:32 +0200

    updated usage to reflect latest core module

--- go.mod
+++ go.mod
@@ -1,27 +1,28 @@
 module alethic-ism-usage
 
-go 1.22
+go 1.24
+
+toolchain go1.24.4
+
+require github.com/quantumwake/alethic-ism-core-go v0.1.14
 
 require (
-	github.com/nats-io/nats.go v1.37.0
-	github.com/quantumwake/alethic-ism-core-go v0.0.0-20240902001609-84f22ee5f831
-)
-
-require (
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.6.0 // indirect
-	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jackc/pgx/v5 v5.7.4 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/compress v1.17.2 // indirect
-	github.com/nats-io/nkeys v0.4.7 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/nats-io/nats.go v1.42.0 // indirect
+	github.com/nats-io/nkeys v0.4.11 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/postgres v1.5.9 // indirect
-	gorm.io/gorm v1.25.11 // indirect
+	gorm.io/driver/postgres v1.5.11 // indirect
+	gorm.io/gorm v1.26.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,66 +3,81 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"github.com/nats-io/nats.go"
-	"github.com/quantumwake/alethic-ism-core-go/pkg/data"
+	"github.com/quantumwake/alethic-ism-core-go/pkg/repository/usage"
 	"github.com/quantumwake/alethic-ism-core-go/pkg/routing"
+	"github.com/quantumwake/alethic-ism-core-go/pkg/routing/nats"
 	"log"
 	"os"
 )
 
 var (
-	natsRoute  *routing.NATSRoute
-	dataAccess *data.Access
+	natsRoute *routing.Route
+	backend   *usage.BackendStorage
 )
 
-func onMessageReceived(route *routing.NATSRoute, msg *nats.Msg) {
-	var usage data.Usage
-	err := json.Unmarshal(msg.Data, &usage)
+func onMessageReceived(ctx context.Context, envelop routing.MessageEnvelop) {
+	defer func(envelop routing.MessageEnvelop, ctx context.Context) {
+		err := envelop.Ack(ctx)
+		if err != nil {
+
+		}
+	}(envelop, ctx)
+
+	data, err := envelop.MessageRaw()
 	if err != nil {
+		log.Printf("Error getting raw message: %v", err)
+		return
+	}
+
+	var model usage.Usage
+
+	// unmarshal the message into a usage struct
+	if err = json.Unmarshal(data, &model); err != nil {
 		log.Printf("Error unmarshalling usage: %v", err)
 		return
 	}
 
-	err = dataAccess.InsertUsage(&usage)
+	err = backend.InsertUsage(&model)
 	if err != nil {
 		log.Printf("Error inserting usage: %v", err)
 	}
 
-	err = msg.Ack()
-	if err != nil {
-		return
-	}
 }
 
 func main() {
+	ctx := context.Background()
+
 	// connect to usage database
 	dsn, ok := os.LookupEnv("DSN")
 	if !ok {
 		dsn = "host=localhost port=5432 user=postgres password=postgres1 dbname=postgres sslmode=disable"
 		log.Println("DSN environment variable not set")
 	}
-	dataAccess = data.NewDataAccess(dsn)
+	backend = usage.NewBackend(dsn)
 
-	routingFile, ok := os.LookupEnv("ROUTING_FILE")
-	if !ok {
-		routingFile = "../routing-nats.yaml"
-	}
+	//routingFile, ok := os.LookupEnv("ROUTING_FILE")
+	//if !ok {
+	//	routingFile = "../routing-nats.yaml"
+	//}
 
-	// load the nats routing table
-	routes, err := routing.LoadConfig(routingFile)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//// load the nats routing table
+	//config, err := nats.LoadConfig(routingFile)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
 	// find the usage route we want to listen on
-	route, err := routes.FindRouteBySelector("processor/usage")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//route, err := routes.FindRouteBySelector("processor/usage")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
 	// create a new route and subscribe to inbound messages
-	natsRoute = routing.NewNATSRoute(route, onMessageReceived)
-	err = natsRoute.Subscribe(context.TODO())
+	route, err := nats.NewRouteSubscriberUsingSelector(ctx, "processor/usage", onMessageReceived)
+	if err != nil {
+		return
+	}
+	err = route.Subscribe(ctx)
 	if err != nil {
 		log.Fatalf("unable to subscribe to NATS: %v, route: %v", route, err)
 	}
